2c045aa0d3b871fa2d4b597e5e397d738daa3103
Tyler Barrett <tylerbarrett01@gmail.com>
2021-04-15 17:51:03 -0400

    Added JavaDoc

--- State.java
+++ State.java
@@ -5,9 +5,33 @@
  */
 
 public interface State {
+    /**
+     * Prints the result of pressing the home button and guarantees
+     * the TV will be in the HomeState
+     */
     public void pressHomeButton();
+
+    /**
+     * Prints the result of pressing the Netflix button and guarantees
+     * the TV will be in the NetflixState
+     */
     public void pressNetflixButton();
+
+    /**
+     * Prints the result of pressing the Netflix button and guarantees the TV
+     * will be in the HuluState
+     */
     public void pressHuluButton();
+
+    /**
+     * Will print out all the available movies if applicable, otherwise
+     * prints an error message
+     */
     public void pressMovieButton();
+
+    /**
+     * Will print out all the available television shows if applicable, otherwise
+     * prints an error message
+     */
     public void pressTVButton();
 }
--- TV.java
+++ TV.java
@@ -9,6 +9,9 @@ public class TV {
     private State HuluState;
     private State state;
 
+    /**
+     * Constructs the TV setting the current state to the home state
+     */
     public TV() {
         HomeState = new HomeState(this);
         NetflixState = new NetflixState(this);
@@ -16,38 +19,74 @@ public class TV {
         state = HomeState;
     }
 
+    /**
+     * Will call the method for pressing the home button depending on what state the 
+     * TV is in
+     */
     public void pressHomeButton() {
         state.pressHomeButton();
     }
 
+    /**
+     * Calls the method for pressing the netflix button depending
+     * on what state the TV is in
+     */
     public void pressNetflixButton() {
         state.pressNetflixButton();
     }
 
+    /**
+     * Calls the method for pressing the hulu button
+     * depending on what state the TV is in
+     */
     public void pressHuluButton() {
         state.pressHuluButton();
     }
 
+    /**
+     * Calls the method for pressing the Movie button
+     * depending on what state the TV is in
+     */
     public void pressMovieButton() {
         state.pressMovieButton();
     }
 
+    /**
+     * Calls the method for pressing the 
+     * TV button depending on what state the TV is in
+     */
     public void pressTVButton() {
         state.pressTVButton();
     }
 
+    /**
+     * Sets the state of the TV to the incoming param
+     * @param state used to determine the state the TV will be set to
+     */
     public void setState(State state) {
         this.state = state;
     }
 
+    /**
+     * Returns the instance of the HomeState of the TV
+     * @return HomeState for this TV object
+     */
     public State getHomeState() {
         return this.HomeState;
     }
 
+    /**
+     * Returns the instance fo the NetflixState of the TV
+     * @return NetflixState for this TV object
+     */
     public State getNetflixState() {
         return this.NetflixState;
     }
 
+    /**
+     * Returns the instance of the HuluState of the TV
+     * @return HuluState for this TV object
+     */
     public State getHuluState() {
         return this.HuluState;
     }
